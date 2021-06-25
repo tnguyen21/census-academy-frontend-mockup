a18f67e0b1612b9a306e1de3960fa7431e05f376
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-06-25 09:43:32 -0500

    feat: add footer with temp links
    write Footer component tto be used across the site
    
    links in the footer are not set, just temporarily like that since this
    is a mock up and we are not expecting to use this right away
    
    place holder email sign up space -- need to write this component with
    appropriate logic and accessibility checks
    
    installed font awesome to have branding svgs

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,6 +1,24 @@
 import React from "react";
 import { Container, Grid } from "@material-ui/core";
-import { StyledFooter } from "./style";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faInstagram,
+  faTwitter,
+  faYoutube,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  StyledFooter,
+  FooterHeader,
+  FooterLinks,
+  LinkItem,
+  Link,
+  Span,
+  Slogan,
+  OutboundLinks,
+  CenteredIcon,
+} from "./style";
 
 function Footer() {
   return (
@@ -12,13 +30,84 @@ function Footer() {
             <p>
               To sign up for updates please enter your contact information below
             </p>
+            <div>Email Subscription Here</div>
           </Grid>
           <Grid item xs={2}>
-            <h2>Stay Current</h2>
+            <FooterHeader>Stay Current</FooterHeader>
+            {/* TODO check this for accessibility */}
+            <FooterLinks>
+              <LinkItem>
+                <Link href="https://www.census.gov/newsroom.html">
+                  Newsroom
+                </Link>
+              </LinkItem>
+              <LinkItem>
+                <Link href="https://www.census.gov/library/stories.html">
+                  America Counts
+                </Link>
+              </LinkItem>
+              <LinkItem>
+                <Link href="https://www.census.gov/newsroom/blogs.html">
+                  Blogs
+                </Link>
+              </LinkItem>
+              <LinkItem>
+                <Link href="https://www.census.gov/newsroom/stories.html">
+                  Stats for Stories
+                </Link>
+              </LinkItem>
+            </FooterLinks>
           </Grid>
           <Grid item xs={2}>
             <h2>Stay Connected</h2>
+            <Grid container spacing={2}>
+              <CenteredIcon item xs={4}>
+                <Link href="#">
+                  <FontAwesomeIcon icon={faFacebookF} />
+                </Link>
+              </CenteredIcon>
+              <CenteredIcon item xs={4}>
+                <Link href="#">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </Link>
+              </CenteredIcon>
+              <CenteredIcon item xs={4}>
+                <Link href="#">
+                  <FontAwesomeIcon icon={faLinkedinIn} />
+                </Link>
+              </CenteredIcon>
+              <CenteredIcon item xs={6}>
+                <Link href="#">
+                  <FontAwesomeIcon icon={faYoutube} />
+                </Link>
+              </CenteredIcon>
+              <CenteredIcon item xs={6}>
+                <Link href="#">
+                  <FontAwesomeIcon icon={faInstagram} />
+                </Link>
+              </CenteredIcon>
+            </Grid>
           </Grid>
+        </Grid>
+        <OutboundLinks justify="center" container spacing={2}>
+          <Link href="#">Census Jobs</Link>
+          <Span>|</Span>
+          <Link href="#">Information Quality</Link>
+          <Span>|</Span>
+          <Link href="#">Data Linkage Infrastructure</Link>
+          <Span>|</Span>
+          <Link href="#">Data Protection and Privacy Policy</Link>
+          <Span>|</Span>
+          <Link href="#">Accessibility</Link>
+          <Span>|</Span>
+          <Link href="#">FOIA</Link>
+          <Span>|</Span>
+          <Link href="#">U.S. Department of Commerce</Link>
+          <Span>|</Span>
+          <Link href="#">USA.gov</Link>
+        </OutboundLinks>
+        <Grid justify="center" container spacing={2}>
+          <Slogan>Measuring America's People, Places, and Economy</Slogan>
         </Grid>
       </Container>
     </StyledFooter>
--- src/components/shared/Footer/style.ts
+++ src/components/shared/Footer/style.ts
@@ -1,5 +1,54 @@
 import styled from "styled-components";
+import { Grid } from "@material-ui/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const StyledFooter = styled.footer`
   background: hsl(213 65% 19%);
+  color: white;
+`;
+
+export const FooterHeader = styled.h2`
+  margin-bottom: 8px;
+`;
+
+export const FooterLinks = styled.ul`
+  margin: 0;
+  padding: 0;
+`;
+
+export const LinkItem = styled.li`
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+`;
+
+export const Link = styled.a`
+  color: white;
+  font-weight: 500;
+  text-decoration: none;
+  line-height: 1.8;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const Span = styled.span`
+  margin: 0px 6px;
+`;
+
+export const Slogan = styled.p`
+  font-size: 1.25rem;
+`;
+
+export const OutboundLinks = styled(Grid)`
+  && {
+    padding-bottom: 16px;
+    margin: 32px 0px 8px;
+    border-bottom: thin solid hsl(213deg 28% 35%);
+  }
+`;
+
+export const CenteredIcon = styled(Grid)`
+  font-size: 1.5rem;
+  text-align: center;
 `;
